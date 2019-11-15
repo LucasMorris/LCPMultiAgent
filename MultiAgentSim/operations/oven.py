f8f6ab6821e2249f30093e106fa8fddfc6b8d322
Marc <marc@uni-bremen.de>
2019-11-15 12:12:15 +0100

    current status blueberries

---
+++ MultiAgentSim/operations/oven.py
@@ -0,0 +1,6 @@
+class oven:
+    def bake_bread:
+        if dough >  1 and flour > 2 and salt > 10:
+        dough -= 1
+        flour -= 1
+        bread -= 1
---
+++ MultiAgentSim/stocking_points/stocking_point_incoming.py
@@ -0,0 +1,6 @@
+class incoming_storage:
+    def max_storage:
+        if blueberry > 20:
+            blueberry = 20
+    def ingredient_pull:
+        if :
