df1dc986e0ab0fb8b81402099e3e29543709e3e6
Marc <marc@uni-bremen.de>
2019-11-15 14:07:27 +0100

    ingredients updated self.stock

--- MultiAgentSim/stocking_points/stocking_point_incoming.py
+++ MultiAgentSim/stocking_points/stocking_point_incoming.py
@@ -1,9 +1,16 @@
 from MultiAgentSim.ingredient import blueberries
+from MultiAgentSim.ingredient import chocolate
+from MultiAgentSim.ingredient import dough
+from MultiAgentSim.ingredient import flour
+from MultiAgentSim.ingredient import salt
 from MultiAgentSim import timer
 
 class incoming_storage:
     def __init__(self):
-        self.initial_stock = 0
-        self.stock_blueberries = (self.initial_stock + (blueberries.availibility * timer.clock_t))
-
+        self.initial_stock = [8,7,6,5,4]
+        self.stock_blueberries_new = (self.initial_stock[1] + self.stock_blueberries + (blueberries.availibility * timer.clock_t))
+        self.stock_chocolate_new = (self.initial_stock[2] + self.stock_chocolate + (chocolate.availibility * timer.clock_t))
+        self.stock_dough_new = (self.initial_stock[3] + self.stock_doug + (dough.availibility * timer.clock_t))
+        self.stock_flour_new = (self.initial_stock[4] + self.stock_flour + (flour.availibility * timer.clock_t))
+        self.stock_salt_new = (self.initial_stock[5] + self.stock_salt (salt.availibility * timer.clock_t))
 
