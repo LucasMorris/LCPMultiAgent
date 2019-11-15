a193e2f6a93c7450e62d599278c78a02ad0e9ed2
Marc <marc@uni-bremen.de>
2019-11-15 14:29:56 +0100

    list clock

--- MultiAgentSim/stocking_points/stocking_point_incoming.py
+++ MultiAgentSim/stocking_points/stocking_point_incoming.py
@@ -8,9 +8,15 @@ from MultiAgentSim import timer
 class incoming_storage:
     def __init__(self):
         self.initial_stock = [8,7,6,5,4]
-        self.stock_blueberries_new = (self.initial_stock[1] + self.stock_blueberries + (blueberries.availibility * timer.clock_t))
-        self.stock_chocolate_new = (self.initial_stock[2] + self.stock_chocolate + (chocolate.availibility * timer.clock_t))
-        self.stock_dough_new = (self.initial_stock[3] + self.stock_doug + (dough.availibility * timer.clock_t))
-        self.stock_flour_new = (self.initial_stock[4] + self.stock_flour + (flour.availibility * timer.clock_t))
-        self.stock_salt_new = (self.initial_stock[5] + self.stock_salt (salt.availibility * timer.clock_t))
+        self.stock_blueberries_accumulated = (self.initial_stock[0] + (blueberries.availibility * timer.clock_t))
+        self.stock_chocolate_accumulated = (self.initial_stock[1] + (chocolate.availibility * timer.clock_t))
+        self.stock_dough_accumulated = (self.initial_stock[2] + (dough.availibility * timer.clock_t))
+        self.stock_flour_accumulated = (self.initial_stock[3] + (flour.availibility * timer.clock_t))
+        self.stock_salt_accumulated = (self.initial_stock[4] + (salt.availibility * timer.clock_t))
 
+
+
+stock_bb = []
+if timer.clock_t < 0:
+    stock_bb[timer.clock_t] = stock_bb[timer.clock_t - 1] + push[timer.clock_t] - pull[timer.clock_t]
+else stock_bb[timer.clock_t] = push[timer.clock_t] - pull[timer.clock_t]
