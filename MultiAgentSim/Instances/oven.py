81d8681145ada87bd7e79632c53e096db365121d
Marc <marc@uni-bremen.de>
2019-11-26 15:26:21 +0100

    Ein paar Ideen

--- MultiAgentSim/Instances/oven.py
+++ MultiAgentSim/Instances/oven.py
@@ -2,6 +2,9 @@ from stockingpoint import current_stock
 
 def status():
     if t % 5 == 0:
+        readiness = ready
         print("ready")
     else:
+        readiness = busy
         print("busy")
+
---
+++ MultiAgentSim/Instances/stockingpoint.py
@@ -0,0 +1,6 @@
+def initial_stock():
+    current_stock = 5
+    stock_before = 5
+
+def minimum_stock():
+    
